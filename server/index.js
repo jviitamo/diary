eecbcbb02c596673660616d706d96f54394a6d06
jviitamo <jviitamo@gmail.com>
2022-07-07 10:43:57 +0300

    Update gitignore

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 var storage = multer.diskStorage({   
   destination: function(req, file, cb) { 
-     cb(null, '../frontend/public/');    
+     cb(null, '../frontend/public/photos');    
   }, 
   filename: function (req, file, cb) { 
      cb(null , file.originalname);   
